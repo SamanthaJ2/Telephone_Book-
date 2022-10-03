6d51c083529ede7b7dffe6c979be09d25fbdd778
SamanthaJ2 <samanthamariejae@gmail.com>
2022-10-03 10:21:26 -0400

    firstpush

---
+++ Telephone Book/PhoneBook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephone_Book
+{
+    class PhoneBook
+    {
+        private List<Contact> _contacts { get; set; } = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+            if(contact != null)
+            {
+                Console.WriteLine("Contact not found");
+            }
+            else
+            {
+                Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+            }
+        }
+        public void DisplayAllContacts()
+        {
+            foreach (var contact in _contacts)
+            {
+                Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+            }
+        }
+
+    } 
+}
